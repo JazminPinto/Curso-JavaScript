6f1cc62aa11669e481da54d17c14a37b6f170830
jazmin pinto <jazminpinto61@gmail.com>
2023-01-17 14:00:10 -0300

    first commit

---
+++ projeto1.js
@@ -0,0 +1,13 @@
+var miSustantivo = 'perro';
+var miAdjetivo = 'negro';
+var miVerbo = 'corrio';
+var miAdverbio = 'rapidamente';
+
+/* Concatena las cadenas para crear una cadena que muestre un mensaje.
+Puedes cambiar los valores de las variables.
+Por ejemplo: El perro negro corrio rapidamente a la tienda.
+             La bicicleta pequeña volo a la tienda lentamente.
+*/
+
+var palabrasEnBlanco = 'El ' + miSustantivo + ' de color '+miAdjetivo + ' con manchas blancas '+ miVerbo + ' ' + miAdverbio + ' por esa calle.' ;
+console.log(palabrasEnBlanco);
---
+++ projeto2.js
@@ -0,0 +1,16 @@
+var miArreglo = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+/*
+Arreglo:        [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Indices:             0          1          2
+Indices internos: 0  1  2    0  1  2    0  1  2
+*/
+
+console.log(miArreglo[0]); // [1, 2, 3]
+console.log(miArreglo[1]); // [4, 5, 6] 
+console.log(miArreglo[2]); // [7, 8, 9]
+//para entrar al arreglo anidado principal
+
+console.log(miArreglo[0][1]); // 2 
+console.log(miArreglo[1][1]); // 5 
+console.log(miArreglo[2][2]); // 9 
+//para entrar al elemento del arreglo anidado
