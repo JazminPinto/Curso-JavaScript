bca446592358c8bd06c6775ff99ebf91f95622ae
jazmin pinto <jazminpinto61@gmail.com>
2023-01-23 17:11:43 -0300

    > mayor que

--- projeto4.1.js
+++ projeto4.1.js
@@ -33,3 +33,20 @@ console.log([1, 2, 3] != [1, 2, 3]); //true
 console.log(1 != '1'); //false (desigualdad = dato comun)
 
 console.log(1 !== '1'); //true (desigualdad estricta = dato no comun)
+
+
+console.log(6 > 5); //true
+console.log(3 > 10); //false
+
+console.log('B' > 'A'); //true
+console.log("ACB" > "ABC"); //true
+
+console.log('AB' > 'A'); //true
+
+console.log("Mundo" > 'Hola'); //true
+
+var a = 15;
+var b = 17;
+
+console.log(a > b); //true
+console.log(b > a); //false
