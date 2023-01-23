d90f9cf0140f3e0e2d7b68e7bdd76af9881b2890
jazmin pinto <jazminpinto61@gmail.com>
2023-01-23 17:17:15 -0300

    < menor que

--- projeto4.1.js
+++ projeto4.1.js
@@ -52,4 +52,17 @@ console.log(a > b); //true
 console.log(b > a); //false
 
 console.log(5 > 5); //false
-console.log(5 >= 5); //true
+console.log(5 >= 5); //true (mayor o igual que)
+
+console.log(5 < 6); //true
+console.log(10 < 3); //false
+
+console.log('A' < 'B'); //true
+console.log('ABC' < 'ACB'); //true
+
+var a = 15;
+var b = 7;
+
+console.log(a < b); //false
+console.log(b < a); //true
+
