8ef42703cfbbc71e0ba5ab5a6ec0020b3802fe89
jazmin pinto <jazminpinto61@gmail.com>
2023-01-23 16:56:02 -0300

    !== desigualdad estricta

--- projeto4.1.js
+++ projeto4.1.js
@@ -28,3 +28,8 @@ console.log(9 != 9); //false
 console.log('JavaScript' != 'JavaScript'); //false
 
 console.log([1, 2, 3] != [1, 2, 3]); //true
+
+
+console.log(1 != '1'); //false (desigualdad = dato comun)
+
+console.log(1 !== '1'); //true (desigualdad estricta = dato no comun)
