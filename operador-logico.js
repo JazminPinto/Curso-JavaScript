adf1245acd389a28458a5eb23dfac8b2a94763eb
jazmin pinto <jazminpinto61@gmail.com>
2023-02-11 13:06:21 -0300

    Condicionales: Orden Logico

--- operador-logico.js
+++ operador-logico.js
@@ -153,3 +153,19 @@ function classificarValor(valor) {
 classificarValor(2);
 classificarValor(15);
 classificarValor(7);
+
+
+function clasificarValor(valor){
+    if(valor < 5){
+        console.log('Menor que 5.')
+    } else if (valor < 10){
+        console.log('Menor que 10.');
+    } else {
+        console.log('Mayor o igual a 10')
+    }
+}
+
+clasificarValor(2);
+clasificarValor(7);
+clasificarValor(15);
+
