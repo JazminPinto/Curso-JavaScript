22c5bbaf50f704dc80b1d1ca6424b2c29794dfd1
jazmin pinto <jazminpinto61@gmail.com>
2023-02-12 13:42:15 -0300

    crear objetos

---
+++ crear-objetos.js
@@ -0,0 +1,25 @@
+/*var miPerro = {
+    'nombre': 'Gino',
+    'edad': 5,
+    'peso': 6,
+    'raza': "Beagle"
+};
+
+var miObjeto = {
+    5:'cinco'
+};
+*/
+
+//Acceder a propiedades: Notacion de punto.
+
+var miPerro = {
+    'nombre': 'Gino',
+    'edad': 5,
+    'peso': 6,
+    'raza': "Beagle"
+};  
+
+console.log('Mi perro se llama ' + miPerro.nombre);
+console.log(miPerro.edad + ' meses de edad');
+console.log(miPerro.peso + ' kilos');
+console.log(miPerro.raza);
