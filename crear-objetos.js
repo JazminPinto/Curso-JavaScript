eb8364ed7b983fb552036302f371949b54555d0f
jazmin pinto <jazminpinto61@gmail.com>
2023-02-12 13:49:01 -0300

    notacion de corchetes.

--- crear-objetos.js
+++ crear-objetos.js
@@ -23,3 +23,24 @@ console.log('Mi perro se llama ' + miPerro.nombre);
 console.log(miPerro.edad + ' meses de edad');
 console.log(miPerro.peso + ' kilos');
 console.log(miPerro.raza);
+
+
+//Acceder a propiedades: Notacion de corchetes.
+var miCuaderno = {
+    'color': 'verde',
+    'categoria': 3,
+    'numeroDePaginas': 200,
+    'numero de paginas': 200,
+    'numeroDeHojas': 100,
+    'numero de hojas': 100
+};
+console.log(miCuaderno.color);
+console.log(miCuaderno['color']);
+console.log(miCuaderno.categoria);
+console.log(miCuaderno['categoria']);
+console.log(miCuaderno.numeroDePaginas);
+console.log(miCuaderno['numero de paginas']);
+console.log(miCuaderno.numeroDeHojas);
+console.log(miCuaderno['numero de hojas']);
+
+
