152a21fffb8b5aa69f58a0a4ddb9a2a63b3c438e
jazmin pinto <jazminpinto61@gmail.com>
2023-02-12 12:32:42 -0300

    sentencias switch completa

--- sentencias-switch.js
+++ sentencias-switch.js
@@ -84,3 +84,30 @@ console.log(seleccionarIdioma(1));
 console.log(seleccionarIdioma(2));
 console.log(seleccionarIdioma(3));
 console.log(seleccionarIdioma(4));
+
+
+function clasificarVolumen(valor){
+    var volumen;
+    switch (valor){
+        case 1:
+            volumen = 'bajo';
+            break;
+        case 2:
+        case 3:
+            volumen = 'intermedio';
+            break;
+        case 4:
+        case 5:
+        case 6:
+            volumen = 'alto';
+            break;
+    }
+    return volumen;
+}
+
+console.log(clasificarVolumen(1));
+console.log(clasificarVolumen(2));
+console.log(clasificarVolumen(3));
+console.log(clasificarVolumen(4));
+console.log(clasificarVolumen(5));
+console.log(clasificarVolumen(6));
