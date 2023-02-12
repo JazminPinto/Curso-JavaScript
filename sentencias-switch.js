70286e3fcbd2fa14431cca24c59d5d17fa1adee5
jazmin pinto <jazminpinto61@gmail.com>
2023-02-12 12:53:59 -0300

    patron de retorno anticipado

--- sentencias-switch.js
+++ sentencias-switch.js
@@ -175,3 +175,24 @@ function esMenorQue(a,b){
 }
 console.log(esMenorQue(3, 6))
 console.log(esMenorQue(6, 3))
+
+
+
+//Patron de retorno anticipado
+
+function miFuncion(){
+    console.log('Hola');
+    return 'Mundo';
+    console.log('Adios');
+}
+console.log(miFuncion());
+
+
+function calcularRaizCuadrada(num) {
+    if (num < 0) {
+        return undefined;
+    }
+    return Math.sqrt(num);
+}
+console.log(calcularRaizCuadrada(25));
+console.log(calcularRaizCuadrada(-5));
