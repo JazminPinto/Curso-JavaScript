313c9fbd429283a8427b833d661046e17c4d0b65
jazmin pinto <jazminpinto61@gmail.com>
2023-02-12 15:01:27 -0300

    objetos para busquedas

--- crear-objetos.js
+++ crear-objetos.js
@@ -106,3 +106,55 @@ delete curso.duracion;
 console.log(curso);
 
 
+//Objetos para busquedas
+
+function buscarElementoQuimico(simbolo){
+    var elementoQuimico = '';
+    switch (simbolo){
+        case 'Al':
+            elementoQuimico = 'Aluminio';
+            break;
+        case 'S':
+            elementoQuimico = 'Azufre';
+            break;        
+        case 'Cl':
+            elementoQuimico = 'Cloro';
+            break;        
+        case 'He':
+            elementoQuimico = 'Helio';
+            break;        
+        case 'B':
+            elementoQuimico = 'Boro';
+            break;   
+        case 'Li':
+            elementoQuimico = 'Litio';
+            break;    
+    }
+    return elementoQuimico;
+}
+console.log(buscarElementoQuimico('Al'));
+console.log(buscarElementoQuimico('S'));
+console.log(buscarElementoQuimico('Cl'));
+console.log(buscarElementoQuimico('He'));
+console.log(buscarElementoQuimico('B'));
+console.log(buscarElementoQuimico('Li'));
+
+//o, para escribir un codigo mas consigo o mas corto con el mismo resultado
+
+function buscarElementoQuimico(simbolo){
+var simbolosQuimicos = {
+    'Al': 'Aluminio',
+    'S': 'Azufre',
+    'Cl': 'Cloro',
+    'He': 'Helio',
+    'B': 'Boro',
+    'Li': 'Litio'
+    };
+    return simbolosQuimicos[simbolo];
+}
+console.log(buscarElementoQuimico('Al'));
+console.log(buscarElementoQuimico('S'));
+console.log(buscarElementoQuimico('Cl'));
+console.log(buscarElementoQuimico('He'));
+console.log(buscarElementoQuimico('B'));
+console.log(buscarElementoQuimico('Li'));
