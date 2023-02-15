dd47a5bcaa4c8b2752d1e4ceb897ff3e34e87fa0
jazmin pinto <jazminpinto61@gmail.com>
2023-02-15 15:54:11 -0300

    ciclo For Anidados

--- ciclo-while.js
+++ ciclo-while.js
@@ -113,3 +113,19 @@ function contarNumerosPares(arreglito) {
     return total;
 }
 console.log(contarNumerosPares([5, 3, 1, 2, 4, 6, 8, 9]));
+
+
+//Ciclos For Anidados
+var arreglo1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+for (var i = 0; i <arreglo1.length; i++) {
+    console.log('> Nueva iteracion:');
+    console.log('Arreglo:');
+    var arregloAnidado = arreglo1[i]; //Arreglo
+
+    for (var j = 0; j < arregloAnidado.length; j++) {
+        console.log('>>> Ciclo anidado');
+        console.log('Elemento:');
+        console.log(arregloAnidado[j]); //Elemento
+    }
+}
